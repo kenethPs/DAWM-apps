608295d5165c9aec97015330682746a86fbe171e
Allan <aavendan@gmail.com>
2022-11-28 12:09:44 -0500

    hito3

---
+++ album/clienteAngular/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ContactoComponent } from './contacto/contacto.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContactoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
