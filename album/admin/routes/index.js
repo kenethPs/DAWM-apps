517ab4010b06da35513b71a1859c4f58ad97e9f6
Allan <aavendan@gmail.com>
2022-12-14 07:43:20 -0500

    hito3-admin

--- album/admin/routes/index.js
+++ album/admin/routes/index.js
@@ -1,13 +1,19 @@
 var express = require('express');
+const axios = require('axios')
 var router = express.Router();
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/photos', function(req, res, next) {
-  res.render('fotos', { title: 'Fotos' });
+router.get('/photos', async function(req, res, next) {
+  
+  const URL = 'https://dawm-fiec-espol-default-rtdb.firebaseio.com/photos.json'
+  const response = await axios.get(URL)
+  
+  res.render('fotos', { title: 'Fotos', fotos: response.data });
 })
 
 module.exports = router;
